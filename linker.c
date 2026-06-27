f99ed61bc1aba66341a6af0f5e18abb3d4fac550
Chris <morenochristopher580@gmail.com>
2026-06-27 12:38:52 -0500

    Create linker.c

---
+++ linker.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <windows.h>
+
+void resolve_imports() {
+    // No imports needed for stub
+}
+
+uint64_t get_import_address(const char* dll, const char* func) {
+    return 0;
+}
