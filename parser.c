c15bb2c7d1e23fe11379e2f904a4029c76be48cd
Chris <morenochristopher580@gmail.com>
2026-06-27 12:42:16 -0500

    Update parser.c

--- parser.c
+++ parser.c
@@ -3,110 +3,73 @@
 #include <stdlib.h>
 #include "lynx.h"
 
-// Forward declarations
 void parse_statement();
 double parse_expression();
 int parse_logic_expression();
 
 double parse_primary() {
     Token t = scanToken();
-
-    if (t.type == TOKEN_NUMBER) {
-        return atof(t.start);
-    }
-    else if (t.type == TOKEN_IDENTIFIER) {
+    if (t.type == TOKEN_NUMBER) return atof(t.start);
+    if (t.type == TOKEN_IDENTIFIER) {
         char name[64];
         snprintf(name, t.length + 1, "%s", t.start);
-
         if (peekToken().type == TOKEN_LPAREN) {
-            scanToken(); // consume (
-            scanToken(); // consume )
+            scanToken(); scanToken();
             return callFunction(name);
         }
-
         return getVar(name);
     }
-    else if (t.type == TOKEN_STRING) {
+    if (t.type == TOKEN_STRING) {
         char str[256];
         snprintf(str, t.length - 1, "%s", t.start + 1);
         printf("%s\n", str);
         return 0;
     }
-    else if (t.type == TOKEN_LPAREN) {
+    if (t.type == TOKEN_LPAREN) {
         double val = parse_expression();
-        scanToken(); // consume )
+        scanToken();
         return val;
     }
-    else if (t.type == TOKEN_NOT) {
-        return !parse_primary();
-    }
-
+    if (t.type == TOKEN_NOT) return !parse_primary();
     return 0;
 }
 
 double parse_term() {
     double value = parse_primary();
-
     Token op = peekToken();
     while (op.type == TOKEN_STAR || op.type == TOKEN_SLASH || op.type == TOKEN_MODULO) {
         scanToken();
         double right = parse_primary();
-
         switch (op.type) {
             case TOKEN_STAR: value *= right; break;
-            case TOKEN_SLASH:
-                if (right == 0) {
-                    printf("🐾 Can't divide by zero!\n");
-                    return 0;
-                }
-                value /= right;
-                break;
-            case TOKEN_MODULO:
-                if (right == 0) {
-                    printf("🐾 Can't modulo by zero!\n");
-                    return 0;
-                }
-                value = (double)((int)value % (int)right);
-                break;
+            case TOKEN_SLASH: if (right) value /= right; else printf("🐾 Div by zero\n"); break;
+            case TOKEN_MODULO: if (right) value = (double)((int)value % (int)right); break;
             default: break;
         }
-
         op = peekToken();
     }
-
     return value;
 }
 
 double parse_expression() {
     double value = parse_term();
-
     Token op = peekToken();
     while (op.type == TOKEN_PLUS || op.type == TOKEN_MINUS) {
         scanToken();
         double right = parse_term();
-
-        switch (op.type) {
-            case TOKEN_PLUS: value += right; break;
-            case TOKEN_MINUS: value -= right; break;
-            default: break;
-        }
-
+        if (op.type == TOKEN_PLUS) value += right;
+        else value -= right;
         op = peekToken();
     }
-
     return value;
 }
 
 int parse_comparison() {
     double left = parse_expression();
-
     Token op = scanToken();
-    if (op.type == TOKEN_EQ || op.type == TOKEN_NE ||
-        op.type == TOKEN_GT || op.type == TOKEN_LT ||
-        op.type == TOKEN_GE || op.type == TOKEN_LE) {
-
+    if (op.type == TOKEN_EQ || op.type == TOKEN_NE || op.type == TOKEN_GT ||
+        op.type == TOKEN_LT || op.type == TOKEN_GE || op.type == TOKEN_LE) {
         double right = parse_expression();
-
         switch (op.type) {
             case TOKEN_EQ: return left == right;
             case TOKEN_NE: return left != right;
@@ -117,92 +80,65 @@ int parse_comparison() {
             default: return 0;
         }
     }
-
     return 0;
 }
 
 int parse_logic_expression() {
     int left = parse_comparison();
-
     Token op = peekToken();
     while (op.type == TOKEN_AND || op.type == TOKEN_OR) {
         scanToken();
         int right = parse_comparison();
-
-        switch (op.type) {
-            case TOKEN_AND: left = left && right; break;
-            case TOKEN_OR: left = left || right; break;
-            default: break;
-        }
-
+        if (op.type == TOKEN_AND) left = left && right;
+        else left = left || right;
         op = peekToken();
     }
-
     return left;
 }
 
 void parse_block() {
     Token brace = scanToken();
     if (brace.type != TOKEN_LBRACE) return;
-
-    while (peekToken().type != TOKEN_RBRACE && peekToken().type != TOKEN_EOF) {
+    while (peekToken().type != TOKEN_RBRACE && peekToken().type != TOKEN_EOF)
         parse_statement();
-    }
-    scanToken(); // consume }
+    scanToken();
 }
 
 void parse_for_loop() {
     Token varToken = scanToken();
     if (varToken.type != TOKEN_IDENTIFIER) return;
-
     char varName[64];
     snprintf(varName, varToken.length + 1, "%s", varToken.start);
-
     Token eq = scanToken();
     if (eq.type != TOKEN_EQUAL) return;
-
     double start = parse_expression();
     setVar(varName, start);
-
     Token toToken = scanToken();
     if (toToken.type != TOKEN_IDENTIFIER || strncmp(toToken.start, "To", 2) != 0) return;
-
     double end = parse_expression();
-
     Token lbrace = scanToken();
     if (lbrace.type != TOKEN_LBRACE) return;
-
     Scanner loopStart = scanner;
-
     for (double i = start; i <= end; i++) {
         setVar(varName, i);
         scanner = loopStart;
-
-        while (peekToken().type != TOKEN_RBRACE && peekToken().type != TOKEN_EOF) {
+        while (peekToken().type != TOKEN_RBRACE && peekToken().type != TOKEN_EOF)
             parse_statement();
-        }
     }
-
-    scanToken(); // consume }
+    scanToken();
 }
 
 void parse_while_loop() {
     int condition = parse_logic_expression();
-
     Token lbrace = scanToken();
     if (lbrace.type != TOKEN_LBRACE) return;
-
     Scanner loopStart = scanner;
-
     while (condition) {
         scanner = loopStart;
-
-        while (peekToken().type != TOKEN_RBRACE && peekToken().type != TOKEN_EOF) {
+        while (peekToken().type != TOKEN_RBRACE && peekToken().type != TOKEN_EOF)
             parse_statement();
-        }
-
         condition = parse_logic_expression();
-        scanToken(); // consume }
+        scanToken();
         lbrace = scanToken();
         if (lbrace.type != TOKEN_LBRACE) break;
         loopStart = scanner;
@@ -212,72 +148,53 @@ void parse_while_loop() {
 void parse_function_def() {
     Token nameToken = scanToken();
     if (nameToken.type != TOKEN_IDENTIFIER) return;
-
     char funcName[64];
     snprintf(funcName, nameToken.length + 1, "%s", nameToken.start);
-
     Token lparen = scanToken();
     if (lparen.type != TOKEN_LPAREN) return;
-
     char params[10][64];
     int paramCount = 0;
-
     while (peekToken().type != TOKEN_RPAREN && peekToken().type != TOKEN_EOF) {
         Token param = scanToken();
         if (param.type == TOKEN_IDENTIFIER) {
             snprintf(params[paramCount], param.length + 1, "%s", param.start);
             paramCount++;
         }
-
-        if (peekToken().type == TOKEN_COMMA) {
-            scanToken();
-        }
+        if (peekToken().type == TOKEN_COMMA) scanToken();
     }
-
-    scanToken(); // consume )
-
+    scanToken();
     Scanner bodyStart = scanner;
     Token brace = scanToken();
     if (brace.type != TOKEN_LBRACE) return;
-
     int braceCount = 1;
     while (braceCount > 0 && peekToken().type != TOKEN_EOF) {
         Token t = scanToken();
         if (t.type == TOKEN_LBRACE) braceCount++;
         if (t.type == TOKEN_RBRACE) braceCount--;
     }
-
     int bodyLen = (int)(scanner.current - bodyStart.current);
     char* body = malloc(bodyLen + 1);
     strncpy(body, bodyStart.current, bodyLen);
     body[bodyLen] = '\0';
-
     defineFunction(funcName, (const char**)params, paramCount, body);
 }
 
 void parse_statement() {
     Token t = scanToken();
 
-    if (t.type == TOKEN_HUNT) {
-        hunt();
-        return;
-    }
+    if (t.type == TOKEN_HUNT) { hunt(); return; }
 
     if (t.type == TOKEN_ROAR) {
         Token val = scanToken();
         if (val.type == TOKEN_STRING) {
-            for(int i = 1; i < val.length - 1; i++) printf("%c", val.start[i]);
+            for (int i = 1; i < val.length - 1; i++) printf("%c", val.start[i]);
             printf("\n");
         } else if (val.type == TOKEN_IDENTIFIER) {
             char name[64];
             snprintf(name, val.length + 1, "%s", val.start);
-            double num = getVar(name);
-            char* str = getVarString(name);
-            if (str != NULL && strlen(str) > 0) {
-                printf("%s\n", str);
-            } else {
-                printf("%.5f\n", num);
-            }
+            char* s = getVarString(name);
+            if (s && strlen(s) > 0) printf("%s\n", s);
+            else printf("%.5f\n", getVar(name));
         } else if (val.type == TOKEN_NUMBER) {
             printf("%.5f\n", atof(val.start));
         }
@@ -287,28 +204,22 @@ void parse_statement() {
     if (t.type == TOKEN_SET) {
         Token nameToken = scanToken();
         if (nameToken.type != TOKEN_IDENTIFIER) return;
-
         char varName[64];
         snprintf(varName, nameToken.length + 1, "%s", nameToken.start);
-
         Token op = scanToken();
         if (op.type == TOKEN_EQUAL) {
-            // Check if next token is a string
-            Token next = peekToken();
-            if (next.type == TOKEN_STRING) {
-                scanToken(); // consume string
-                char strVal[256];
-                snprintf(strVal, next.length - 1, "%s", next.start + 1);
-                setVarString(varName, strVal);
+            if (peekToken().type == TOKEN_STRING) {
+                Token str = scanToken();
+                char s[256];
+                snprintf(s, str.length - 1, "%s", str.start + 1);
+                setVarString(varName, s);
             } else {
-                double finalVal = parse_expression();
-                setVar(varName, finalVal);
+                double val = parse_expression();
+                setVar(varName, val);
             }
-        }
-        else if (op.type == TOKEN_INCREMENT) {
+        } else if (op.type == TOKEN_INCREMENT) {
             setVar(varName, getVar(varName) + 1);
-        }
-        else if (op.type == TOKEN_DECREMENT) {
+        } else if (op.type == TOKEN_DECREMENT) {
             setVar(varName, getVar(varName) - 1);
         }
         return;
@@ -327,9 +238,9 @@ void parse_statement() {
     if (t.type == TOKEN_POUNCE) {
         Token nameToken = scanToken();
         if (nameToken.type == TOKEN_IDENTIFIER) {
-            char varName[64];
-            snprintf(varName, nameToken.length + 1, "%s", nameToken.start);
-            pounce(varName);
+            char name[64];
+            snprintf(name, nameToken.length + 1, "%s", nameToken.start);
+            pounce(name);
         }
         return;
     }
@@ -337,14 +248,14 @@ void parse_statement() {
     if (t.type == TOKEN_LOAD_LIB) {
         Token libToken = scanToken();
         if (libToken.type == TOKEN_STRING) {
-            char libName[64];
-            snprintf(libName, libToken.length - 1, "%s", libToken.start + 1);
-            load_lib(libName);
+            char lib[64];
+            snprintf(lib, libToken.length - 1, "%s", libToken.start + 1);
+            load_lib(lib);
         }
         return;
     }
 
-    // File I/O
+    // --- File I/O ---
     if (t.type == TOKEN_KITTY_WRITE_FILE) {
         Token path = scanToken();
         Token content = scanToken();
@@ -372,7 +283,6 @@ void parse_statement() {
                 fread(buf, 1, size, f);
                 buf[size] = '\0';
                 fclose(f);
-                // Store in a special variable for now
                 setVarString("__file_content", buf);
                 printf("%s\n", buf);
                 free(buf);
@@ -401,40 +311,8 @@ void parse_statement() {
             char p[256];
             snprintf(p, path.length - 1, "%s", path.start + 1);
             FILE* f = fopen(p, "r");
-            if (f) {
-                fclose(f);
-                setVar("__result", 1.0);
-            } else {
-                setVar("__result", 0.0);
-            }
-        }
-        return;
-    }
-
-    if (t.type == TOKEN_KITTY_LIST_FILES) {
-        Token path = scanToken();
-        if (path.type == TOKEN_STRING) {
-            char p[256];
-            snprintf(p, path.length - 1, "%s", path.start + 1);
-            #ifdef _WIN32
-                char cmd[512];
-                sprintf(cmd, "dir /b \"%s\"", p);
-                system(cmd);
-            #else
-                char cmd[512];
-                sprintf(cmd, "ls -1 \"%s\"", p);
-                system(cmd);
-            #endif
-        }
-        return;
-    }
-
-    if (t.type == TOKEN_KITTY_REMOVE_FILE) {
-        Token path = scanToken();
-        if (path.type == TOKEN_STRING) {
-            char p[256];
-            snprintf(p, path.length - 1, "%s", path.start + 1);
-            remove(p);
+            setVar("__result", f ? 1.0 : 0.0);
+            if (f) fclose(f);
         }
         return;
     }
@@ -449,17 +327,15 @@ void parse_statement() {
         return;
     }
 
-    // Control flow
+    // --- Control Flow ---
     if (t.type == TOKEN_IF) {
-        int condition = parse_logic_expression();
-
-        if (condition) {
+        int cond = parse_logic_expression();
+        if (cond) {
             parse_block();
         } else {
             Scanner save = scanner;
             parse_block();
             scanner = save;
-
             if (peekToken().type == TOKEN_ELSE) {
                 scanToken();
                 parse_block();
@@ -468,20 +344,8 @@ void parse_statement() {
         return;
     }
 
-    if (t.type == TOKEN_FOR) {
-        parse_for_loop();
-        return;
-    }
-
-    if (t.type == TOKEN_WHILE) {
-        parse_while_loop();
-        return;
-    }
-
-    if (t.type == TOKEN_FUNC) {
-        parse_function_def();
-        return;
-    }
-
+    if (t.type == TOKEN_FOR) { parse_for_loop(); return; }
+    if (t.type == TOKEN_WHILE) { parse_while_loop(); return; }
+    if (t.type == TOKEN_FUNC) { parse_function_def(); return; }
     if (t.type == TOKEN_HELP || t.type == TOKEN_EOF) return;
 }
