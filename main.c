73e33c9daa57bc57ad486a3ffd73e3ec920ddede
Chris <morenochristopher580@gmail.com>
2026-07-18 11:34:47 -0600

    Update main.c

--- main.c
+++ main.c
@@ -96,27 +96,8 @@ void runFile(const char* path, int argc, char** argv) {
         buf[size] = '\0';
         fclose(file);
 
-        // ─── SAVE VARIABLE STATE ──────────────────────────────
-        int savedVarCount = varCount;
-        Variable* savedDen = NULL;
-        if (varCount > 0) {
-            savedDen = malloc(varCount * sizeof(Variable));
-            if (savedDen) {
-                for (int i = 0; i < varCount; i++) {
-                    savedDen[i] = den[i];
-                    // Deep copy strings
-                    if (den[i].type == VAR_STRING && den[i].value.strValue) {
-                        savedDen[i].value.strValue = strdup(den[i].value.strValue);
-                    }
-                    if (den[i].type == VAR_ARRAY) {
-                        // For arrays, we'd need to deep copy - skip for now
-                        savedDen[i].value.array = NULL;
-                        savedDen[i].array_length = 0;
-                        savedDen[i].array_capacity = 0;
-                    }
-                }
-            }
-        }
+        // Save variables to temp file BEFORE running script
+        save_vars_to_temp();
 
         // Strip UTF-8 BOM (EF BB BF)
         if (size >= 3 &&
@@ -139,40 +120,8 @@ void runFile(const char* path, int argc, char** argv) {
         }
         scanner = previousScanner;
 
-        // ─── RESTORE VARIABLE STATE ────────────────────────────
-        // Clean up current variables
-        for (int i = 0; i < varCount; i++) {
-            if (den[i].type == VAR_STRING && den[i].value.strValue) {
-                free(den[i].value.strValue);
-                den[i].value.strValue = NULL;
-            }
-            if (den[i].type == VAR_ARRAY) {
-                for (int j = 0; j < den[i].array_capacity; j++) {
-                    if (den[i].value.array[j]) {
-                        if (den[i].value.array[j]->type == VAR_STRING && 
-                            den[i].value.array[j]->value.strValue) {
-                            free(den[i].value.array[j]->value.strValue);
-                            den[i].value.array[j]->value.strValue = NULL;
-                        }
-                        free(den[i].value.array[j]);
-                        den[i].value.array[j] = NULL;
-                    }
-                }
-                free(den[i].value.array);
-                den[i].value.array = NULL;
-            }
-        }
-        
-        // Restore saved variables
-        if (savedDen) {
-            for (int i = 0; i < savedVarCount; i++) {
-                den[i] = savedDen[i];
-            }
-            varCount = savedVarCount;
-            free(savedDen);
-        } else {
-            varCount = 0;
-        }
+        // Restore variables from temp file
+        load_vars_from_temp();
 
         free(buf);
     }
@@ -223,127 +172,3 @@ int main(int argc, char* argv[]) {
                 setVarString("__project_name", argv[2]);
             } else {
                 setVarString("__project_name", "my_project");
-            }
-            if (argc >= 4) {
-                setVarString("__author", argv[3]);
-            } else {
-                setVarString("__author", "Anonymous");
-            }
-            runFile("scripts/init.lnx", 0, NULL);
-            return 0;
-        } else if (STRICMP(argv[1], "add") == 0) {
-            if (argc >= 3) {
-                setVarString("__pkg", argv[2]);
-                runFile("scripts/add.lnx", 0, NULL);
-            } else {
-                setErrorF("Usage: lynx add <package>");
-                fprintf(stderr, "🐾 %s\n", lynx_error);
-                clearError();
-            }
-            return 0;
-        } else if (STRICMP(argv[1], "install") == 0) {
-            runFile("scripts/install.lnx", 0, NULL);
-            return 0;
-        } else if (STRICMP(argv[1], "remove") == 0) {
-            if (argc >= 3) {
-                setVarString("__pkg", argv[2]);
-                runFile("scripts/remove.lnx", 0, NULL);
-            } else {
-                setErrorF("Usage: lynx remove <package>");
-                fprintf(stderr, "🐾 %s\n", lynx_error);
-                clearError();
-            }
-            return 0;
-        } else if (STRICMP(argv[1], "search") == 0) {
-            if (argc >= 3) {
-                setVarString("__term", argv[2]);
-                runFile("scripts/search.lnx", 0, NULL);
-            } else {
-                setErrorF("Usage: lynx search <term>");
-                fprintf(stderr, "🐾 %s\n", lynx_error);
-                clearError();
-            }
-            return 0;
-        } else if (STRICMP(argv[1], "update") == 0) {
-            runFile("scripts/update.lnx", 0, NULL);
-            return 0;
-        } else if (STRICMP(argv[1], "publish") == 0) {
-            runFile("scripts/publish.lnx", 0, NULL);
-            return 0;
-        } else if (STRICMP(argv[1], "build") == 0) {
-            runFile("src/main.lnx", 0, NULL);
-            return 0;
-        } else if (STRICMP(argv[1], "fmt") == 0) {
-            if (argc >= 3) {
-                format_file(argv[2]);
-            } else {
-                setErrorF("Usage: lynx fmt <file.lnx>");
-                fprintf(stderr, "🐾 %s\n", lynx_error);
-                clearError();
-            }
-            return 0;
-        } else if (STRICMP(argv[1], "check") == 0) {
-            if (argc >= 3) {
-                check_file(argv[2]);
-            } else {
-                setErrorF("Usage: lynx check <file.lnx>");
-                fprintf(stderr, "🐾 %s\n", lynx_error);
-                clearError();
-            }
-            return 0;
-        } else {
-            char scriptPath[256];
-            snprintf(scriptPath, sizeof(scriptPath), "scripts/%s.lnx", argv[1]);
-            FILE* f = fopen(scriptPath, "r");
-            if (f) {
-                fclose(f);
-                runFile(scriptPath, 0, NULL);
-            } else {
-                FILE* test = fopen(argv[1], "r");
-                if (test) {
-                    fclose(test);
-                    runFile(argv[1], 0, NULL);
-                } else {
-                    setErrorF("Unknown command: %s", argv[1]);
-                    fprintf(stderr, "🐾 %s\n", lynx_error);
-                    fprintf(stderr, "   Run 'lynx help' for available commands\n");
-                    clearError();
-                }
-            }
-            return 0;
-        }
-
-        unload_all_libs();
-        cleanup_all();
-        return 0;
-    }
-
-    char line[1024];
-    printf("Lynx Engine %s | Type 'Help' for info\n", LYNX_VERSION);
-    while (1) {
-        printf("lynx > ");
-        if (!fgets(line, sizeof(line), stdin)) break;
-        line[strcspn(line, "\n")] = 0;
-        if (strlen(line) == 0) continue;
-
-        if (STRICMP(line, "help") == 0) {
-            show_help();
-        } else if (STRICMP(line, "exit") == 0) {
-            break;
-        } else if (strstr(line, ".lnx") != NULL) {
-            runFile(line, 0, NULL);
-        } else {
-            initScanner(line);
-            parse_statement();
-            if (lynx_error) {
-                fprintf(stderr, "🐾 %s\n", lynx_error);
-                clearError();
-            }
-        }
-    }
-
-    unload_all_libs();
-    cleanup_all();
-    printf("🐾 Goodbye!\n");
-    return 0;
-}
